10111019f63ee29664d85deabd6d35ed12967bca
marc0246 <40955683+marc0246@users.noreply.github.com>
2024-05-26 23:59:13 +0200

    Add benchmarks

---
+++ benches/get.rs
@@ -0,0 +1,45 @@
+#![feature(test)]
+
+extern crate test;
+
+use std::{sync::RwLock, thread};
+use test::{black_box, Bencher};
+
+const ITERATIONS: usize = 100_000;
+const THREADS: usize = 10;
+
+#[bench]
+fn concurrent_slotmap(b: &mut Bencher) {
+    b.iter(|| {
+        let map = concurrent_slotmap::SlotMap::new((ITERATIONS * 2) as u32);
+        let id = map.insert([0usize; 2]);
+
+        thread::scope(|s| {
+            for _ in 0..THREADS {
+                s.spawn(|| {
+                    for _ in 0..black_box(ITERATIONS / THREADS) {
+                        black_box(map.get(black_box(id)));
+                    }
+                });
+            }
+        });
+    });
+}
+
+#[bench]
+fn rwlock_slotmap(b: &mut Bencher) {
+    b.iter(|| {
+        let map = RwLock::new(slotmap::SlotMap::new());
+        let id = map.write().unwrap().insert([0usize; 2]);
+
+        thread::scope(|s| {
+            for _ in 0..THREADS {
+                s.spawn(|| {
+                    for _ in 0..black_box(ITERATIONS / THREADS) {
+                        black_box(map.read().unwrap().get(black_box(id)));
+                    }
+                });
+            }
+        });
+    });
+}
---
+++ benches/insert.rs
@@ -0,0 +1,47 @@
+#![feature(test)]
+
+extern crate test;
+
+use std::{sync::RwLock, thread};
+use test::{black_box, Bencher};
+
+const ITERATIONS: usize = 100_000;
+const THREADS: usize = 10;
+
+#[bench]
+fn concurrent_slotmap(b: &mut Bencher) {
+    b.iter(|| {
+        let map = concurrent_slotmap::SlotMap::new((ITERATIONS * 2) as u32);
+
+        thread::scope(|s| {
+            for _ in 0..THREADS {
+                s.spawn(|| {
+                    for _ in 0..black_box(ITERATIONS / THREADS) {
+                        map.insert(black_box([0usize; 2]));
+                    }
+                });
+            }
+        });
+
+        map
+    });
+}
+
+#[bench]
+fn rwlock_slotmap(b: &mut Bencher) {
+    b.iter(|| {
+        let map = RwLock::new(slotmap::SlotMap::new());
+
+        thread::scope(|s| {
+            for _ in 0..THREADS {
+                s.spawn(|| {
+                    for _ in 0..black_box(ITERATIONS / THREADS) {
+                        map.write().unwrap().insert(black_box([0usize; 2]));
+                    }
+                });
+            }
+        });
+
+        map
+    });
+}
